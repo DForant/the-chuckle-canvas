4b7b52ba0c2e83f9f530d19c1b3980fd3c5e207b
Dean Forant <dforant@gmail.com>
2026-09-21 00:08:43 -0400

    change target of api endpoint

--- client/vite.config.js
+++ client/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://stg.api.thechucklecanvas.com',
+        target: 'http://localhost:4000',
         changeOrigin: true,
         secure: false,
       },
